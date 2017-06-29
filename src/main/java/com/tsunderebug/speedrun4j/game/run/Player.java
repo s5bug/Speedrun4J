fac542bb144e7ae3e0dab6ca02da7047d6da6708
firestar115 <aprime2003@gmail.com>
2017-06-29 00:49:54 -0700

    Get player names

--- src/main/java/com/tsunderebug/speedrun4j/game/run/Player.java
+++ src/main/java/com/tsunderebug/speedrun4j/game/run/Player.java
@@ -1,10 +1,22 @@
 package com.tsunderebug.speedrun4j.game.run;
 
+import com.tsunderebug.speedrun4j.user.User;
+
+import java.io.IOException;
+
 public class Player {
 
 	private String rel;
 	private String id;
 	private String name;
 	private String uri;
+
+	public String getName() throws IOException {
+		if(id != null) {
+			return User.fromID(id).getNames().get("international");
+		} else {
+			return name;
+		}
+	}
 
 }
