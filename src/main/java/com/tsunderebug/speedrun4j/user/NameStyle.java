992ed97b423b18e4d1007d3388fcf61406eae50c
firestar115 <aprime2003@gmail.com>
2017-06-28 18:27:43 -0700

    NameStyle fix

--- src/main/java/com/tsunderebug/speedrun4j/user/NameStyle.java
+++ src/main/java/com/tsunderebug/speedrun4j/user/NameStyle.java
@@ -1,10 +1,14 @@
 package com.tsunderebug.speedrun4j.user;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.util.Map;
 
 public class NameStyle {
 
 	private String style;
 	private Map<String, String> color;
+	@SerializedName("color-from") private Map<String, String> colorFrom;
+	@SerializedName("color-to") private Map<String, String> colorTo;
 
 }
