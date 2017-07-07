d3041ba932f62539d824a4a483766e7dfc1029fd
firestar115 <aprime2003@gmail.com>
2017-07-06 23:45:20 -0700

    get video links

--- src/main/java/com/tsunderebug/speedrun4j/data/Videos.java
+++ src/main/java/com/tsunderebug/speedrun4j/data/Videos.java
@@ -5,4 +5,12 @@ public class Videos {
 	private String text;
 	private Resource[] links;
 
+	public String getText() {
+		return text;
+	}
+
+	public Resource[] getLinks() {
+		return links;
+	}
+
 }
