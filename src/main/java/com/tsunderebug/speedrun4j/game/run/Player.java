dfc7520b66590fc9a72764820f307e4d810f22a1
firestar115 <aprime2003@gmail.com>
2017-07-06 23:41:48 -0700

    Get player color

--- src/main/java/com/tsunderebug/speedrun4j/game/run/Player.java
+++ src/main/java/com/tsunderebug/speedrun4j/game/run/Player.java
@@ -2,6 +2,7 @@ package com.tsunderebug.speedrun4j.game.run;
 
 import com.tsunderebug.speedrun4j.user.User;
 
+import java.awt.*;
 import java.io.IOException;
 
 public class Player {
@@ -16,6 +17,14 @@ public class Player {
 			return User.fromID(id).getNames().get("international");
 		} else {
 			return name;
+		}
+	}
+
+	public Color getColor() throws IOException {
+		if(id != null) {
+			return User.fromID(id).getNameStyle().getColor();
+		} else {
+			return Color.white;
 		}
 	}
 
