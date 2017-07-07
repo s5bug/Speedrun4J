2420eceeef53f4bebb46e536cbcd481aee69f524
firestar115 <aprime2003@gmail.com>
2017-07-06 23:26:09 -0700

    getters

--- src/main/java/com/tsunderebug/speedrun4j/game/GameAssets.java
+++ src/main/java/com/tsunderebug/speedrun4j/game/GameAssets.java
@@ -18,4 +18,52 @@ public class GameAssets {
 	private Asset background;
 	private Asset foreground;
 
+	public Asset getLogo() {
+		return logo;
+	}
+
+	public Asset getCoverTiny() {
+		return coverTiny;
+	}
+
+	public Asset getCoverSmall() {
+		return coverSmall;
+	}
+
+	public Asset getCoverMedium() {
+		return coverMedium;
+	}
+
+	public Asset getCoverLarge() {
+		return coverLarge;
+	}
+
+	public Asset getIcon() {
+		return icon;
+	}
+
+	public Asset getTrophyFirst() {
+		return trophyFirst;
+	}
+
+	public Asset getTrophySecond() {
+		return trophySecond;
+	}
+
+	public Asset getTrophyThird() {
+		return trophyThird;
+	}
+
+	public Asset getTrophyFourth() {
+		return trophyFourth;
+	}
+
+	public Asset getBackground() {
+		return background;
+	}
+
+	public Asset getForeground() {
+		return foreground;
+	}
+
 }
--- src/main/java/com/tsunderebug/speedrun4j/user/NameStyle.java
+++ src/main/java/com/tsunderebug/speedrun4j/user/NameStyle.java
@@ -2,7 +2,11 @@ package com.tsunderebug.speedrun4j.user;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class NameStyle {
 
@@ -10,5 +14,24 @@ public class NameStyle {
 	private Map<String, String> color;
 	@SerializedName("color-from") private Map<String, String> colorFrom;
 	@SerializedName("color-to") private Map<String, String> colorTo;
+
+	public Color getColor() {
+		List<Color> c = color.values().stream().map(s -> new Color(Integer.parseInt(s.substring(1), 16))).collect(Collectors.toList());
+		List<Color> cf = colorFrom.values().stream().map(s -> new Color(Integer.parseInt(s.substring(1), 16))).collect(Collectors.toList());
+		List<Color> ct = colorTo.values().stream().map(s -> new Color(Integer.parseInt(s.substring(1), 16))).collect(Collectors.toList());
+		List<Color> all = new ArrayList<>();
+		all.addAll(c);
+		all.addAll(cf);
+		all.addAll(ct);
+		int r = 0;
+		int g = 0;
+		int b = 0;
+		for(Color col : all) {
+			r += col.getRed();
+			g += col.getGreen();
+			b += col.getBlue();
+		}
+		return new Color(r / all.size(), g / all.size(), b / all.size());
+	}
 
 }
