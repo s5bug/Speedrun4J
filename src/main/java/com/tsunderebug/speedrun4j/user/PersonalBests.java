6984b83f2344bdb4f657fce85d5f6dde91016a52
firestar115 <aprime2003@gmail.com>
2017-07-23 15:47:50 -0700

    HTTPS fix

--- src/main/java/com/tsunderebug/speedrun4j/user/PersonalBests.java
+++ src/main/java/com/tsunderebug/speedrun4j/user/PersonalBests.java
@@ -3,10 +3,10 @@ package com.tsunderebug.speedrun4j.user;
 import com.google.gson.Gson;
 import com.tsunderebug.speedrun4j.Speedrun4J;
 import com.tsunderebug.speedrun4j.game.run.PlacedRun;
-import sun.net.www.protocol.http.HttpURLConnection;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class PersonalBests {
